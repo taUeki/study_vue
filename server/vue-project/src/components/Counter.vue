f217f320ac5d323c381904be9c403f104b5d8e81
植木孝史 <>
2020-01-27 23:40:49 +0900

    first

---
+++ server/vue-project/src/components/Counter.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p>counter</p>
+    <input
+      @click="coutnUp()"
+      type="button"
+      value="+1"
+    />
+    <p>{{count}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    coutnUp () {
+      this.count++
+    }
+  }
+}
+</script>
